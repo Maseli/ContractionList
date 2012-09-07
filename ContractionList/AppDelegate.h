dfd8a790427eabdde65124b0ee96cf83bc2fae87
xuzhongzhou <xuzhongzhou@foxmail.com>
2012-09-07 10:14:31 +0800

    Initial Commit

---
+++ ContractionList/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ContractionList
+//
+//  Created by 许 忠洲 on 12-9-7.
+//  Copyright (c) 2012年 eshore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ContractionList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ContractionList
+//
+//  Created by 许 忠洲 on 12-9-7.
+//  Copyright (c) 2012年 eshore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
